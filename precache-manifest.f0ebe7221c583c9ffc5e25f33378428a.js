f37460c5ba31e519946159e9f29bd1017780c534
Work <tomves111@gmail.com>
2019-10-10 17:57:42 +0200

    Updates

---
+++ precache-manifest.f0ebe7221c583c9ffc5e25f33378428a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c8c257109fad44d658b8f2b3376da147",
+    "url": "/contactmanager/index.html"
+  },
+  {
+    "revision": "68c4c42200304c1a4e42",
+    "url": "/contactmanager/static/css/2.0624914a.chunk.css"
+  },
+  {
+    "revision": "c380c9a1566759ea5af4",
+    "url": "/contactmanager/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "68c4c42200304c1a4e42",
+    "url": "/contactmanager/static/js/2.13a0c04a.chunk.js"
+  },
+  {
+    "revision": "c380c9a1566759ea5af4",
+    "url": "/contactmanager/static/js/main.e9cd570b.chunk.js"
+  },
+  {
+    "revision": "28b54336cd64c84b0021",
+    "url": "/contactmanager/static/js/runtime-main.344dc1de.js"
+  }
+]);
